cf3d21079489fcf51b365b2104e5dd415060bdd0
Aseem Gupta <aseemguptajaipur@gmail.com>
2024-04-21 13:52:31 +0530

    made input simpler and stop re-initialisation of win_list array in function by moving it out of function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from os import system,name
 from time import sleep 
+
 class Player:
     def __init__(self,symbol) -> None:
         self.name = ""
@@ -14,40 +15,10 @@ class Player:
 
 class Grid:
     def __init__(self) -> None:
-        self.mat = [[(row+1,col+1) for col in range(3)] for row in range(3)]
+        self.mat = [[col + row*3 + 1 for col in range(3)] for row in range(3)]
         self.visited = [[False for _ in range(3)] for _ in range(3)]
 
-    def print_mat(self):
-        print()
-        for row in range(3):
-            print(*self.mat[row],sep = "   ")
-        print()
-
-    @staticmethod
-    def is_valid_box(row,col):
-        return  1 <= row <= 3 and 1 <= col <= 3
-
-    def take_input(self):
-        while True:
-            try:
-                row , col = int(input("Enter row: ")),int(input("Enter col: "))
-                if not self.is_valid_box(row,col):
-                    print("\nEnter valid box position:\n")
-                    continue
-
-                row = row - 1
-                col = col - 1
-
-                return row,col
-
-            except Exception as e:
-                print("\n\n",e,"\n\n")
-
-    def combination(self,r1,c1,r2,c2,r3,c3):
-        return self.mat[r1][c1] == self.mat[r2][c2] == self.mat[r3][c3]
-
-    def check_for_win(self):
-        win_list = [
+        self.win_list = [
                     [0,0,0,1,0,2],
                     [1,0,1,1,1,2],  # Horizontal lines
                     [2,0,2,1,2,2],
@@ -58,18 +29,46 @@ class Grid:
 
                     [0,0,1,1,2,2], # Diagnol lines
                     [0,2,1,1,2,0]
-                    ]
+        ]
+        
+    def print_mat(self):
+        print()
+        for row in range(3):
+            print(*self.mat[row],sep = "   ")
+        print()
 
-        for _ in win_list:
+    @staticmethod
+    def is_valid_box(row,col):
+        return  0 <= row <= 2 and 0 <= col <= 2
+    
+    def take_input(self):
+        while True:
+            try:
+                num = int(input("Enter num: "))
+                num = num - 1 
+                row = num // 3
+                col = num % 3
+
+                if not self.is_valid_box(row,col):
+                    print("\nEnter valid box position:\n")
+                    continue
+                return row,col
+
+            except Exception as e:
+                print("\n\n",e,"\n\n")
+
+    def combination(self,r1,c1,r2,c2,r3,c3):
+        return self.mat[r1][c1] == self.mat[r2][c2] == self.mat[r3][c3]
+
+    def check_for_win(self):
+        for _ in self.win_list:
             r1,c1,r2,c2,r3,c3 = _
             if self.combination(r1,c1,r2,c2,r3,c3):
                return True
         return False 
 
-
     def fill_box(self,player:Player):
         system('cls' if name == 'nt' else 'clear')  # Clears the console
-
 
         self.print_mat()
         player.ask_to_move()
@@ -91,7 +90,6 @@ class Grid:
 
     @staticmethod
     def play(grid,player_1:Player,player_2:Player):
-        print(player_1.name,player_2.name)
         count = 0
 
         while count <= 9:
@@ -103,20 +101,18 @@ class Grid:
             if count==9:
                 print(f"\nIt's a tie between {player_1.name} and {player_2.name}")
                 exit()
-
             player_2.ask_to_move()
 
             grid.fill_box(player_2)
             count+=1
 
-
 grid1 = Grid()
 
-player_1 = Player('  X ')
+player_1 = Player('X')
 print("Player 1 ")
 player_1.ask_name()
 
-player_2 = Player('  O ')
+player_2 = Player('O')
 print("Player 2 ")
 player_2.ask_name()
 
